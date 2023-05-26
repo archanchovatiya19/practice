ce102eb639dea384215e983a971bdf483bbc0e7e
Archan <archanchovatiya1@gmail.com>
2023-05-25 22:14:51 -0400

    This is the folder for practice

---
+++ javascript/introduction.js
@@ -0,0 +1,31 @@
+// console.log("Hello World!")
+
+
+// console.log("Addition of three numbers")
+// function addThreeNumbers(a,b,c){
+//     return a+b+c;
+// }
+// let c = addThreeNumbers(1,2,5)
+// console.log(c)
+
+
+// we cannot redeclare let again it wll give an error
+
+//this is not valid
+// let x = 10
+// let x = 20
+
+//this is valid
+// let y = "Archan"
+// y = 20
+
+var a = 45
+var b ="Archan"
+console.log(a)
+console.log(b)
+{
+var a = 56
+console.log(a)
+let b = "Shradhdha"
+console.log(b)
+}
